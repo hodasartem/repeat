c1679fd005ebc997130e96ba3ee9603b7976f859
KhodasArtyom <hodasartem777@gmail.com>
2021-10-02 13:56:16 +0300

    lower

---
+++ task1.py
@@ -0,0 +1,26 @@
+"""
+milk == 3
+butter == 5
+bread == 7
+"""
+
+product = input('Insert the product:\n')
+
+shop_items = {
+    'milk' : 3,
+    'butter' : 5,
+    'bread' : 7
+}
+print(shop_items.get(product.lower(), -1))
+
+# if product == 'milk':
+#     print(3)
+# elif product =='butter':
+#     print(5)
+# elif product == 'bread':
+#     print(7)
+# else:
+#     print(-1)
+
+
+# print(product)
